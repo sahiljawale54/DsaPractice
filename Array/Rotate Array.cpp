fa39d5cb7220e1f37fca589c93cfe21b9408e3c7
Sahil Jawale <20510057.dypit@dypvp.edu.in>
2022-11-27 10:32:49 +0530

    Update Rotate Array.cpp

--- Array/Rotate Array.cpp
+++ Array/Rotate Array.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     
     void rotate(vector<int>& nums, int k) {
-       k = k % nums.size();
-       reverse(nums.begin()  ,  nums.end()-k); // reverse all remaining last k 
+        k = k % nums.size();
         reverse(nums.end() -k,  nums.end());  // reverse last k elements 
+        reverse(nums.begin()  ,  nums.end()-k); // reverse all remaining last k
         reverse(nums.begin(), nums.end());  //reverse all ele
         
     }
