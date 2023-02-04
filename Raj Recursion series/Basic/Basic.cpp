62d44c49bd63fa0b5b161341cc00297ae5d262ab
Sahil Jawale <20510057.dypit@dypvp.edu.in>
2023-02-04 11:09:49 +0530

    Update Basic.cpp

--- Raj Recursion series/Basic/Basic.cpp
+++ Raj Recursion series/Basic/Basic.cpp
@@ -19,3 +19,35 @@ int main(){
     cout<<array[i]<<" ";
   }
 }
+
+// print all subquence
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void func(int arr[] , int i , int n, vector<int> &ans ) {
+    if (i == n) {
+        for (auto ele : ans) {
+            cout << ele << " ";
+        }
+        cout << endl;
+        return;
+    }
+    //add ele in array
+    ans.push_back(arr[i]);
+    //take
+    func(arr , i + 1 , n , ans);
+  
+    ans.pop_back();
+
+    //not take
+    func(arr , i + 1 , n , ans);
+
+}
+
+int main() {
+    int array[3] = {3 , 1, 2};
+    std::vector<int> ans ;
+    func(array , 0 , 3 , ans);
+}
+
